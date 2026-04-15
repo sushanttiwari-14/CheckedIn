703df22eb62cb0ff58fb06c6d199bf39632a4cf9
Sushant Tiwari <sushanttiwari2005@gmail.com>
2026-04-16 02:10:47 +0530

    feat: add anxiety relief detail screen with photo, verdict, map.

---
+++ CheckedIn/View/CheckDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckDetailView.swift
+//  CheckedIn
+//
+//  Created by sushant tiwari on 16/04/26.
+//
+
+import SwiftUI
+
+struct CheckDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckDetailView()
+}
